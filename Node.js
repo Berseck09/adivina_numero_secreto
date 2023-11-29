7b3b7a893bd260ab5d7f2f2d7990542c207fff8d
Berseck09 <151967918+Berseck09@users.noreply.github.com>
2023-11-29 19:43:23 -0300

    Node.js

---
+++ Node.js
@@ -0,0 +1,39 @@
+npm init -y
+{
+  "name": "adivina_numero_secreto",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "readline-sync": "^1.4.10"
+  },
+  "description": ""
+}
+
+PS C:\Users\Facundo\Documents\adivina_numero_secreto> npm install readline-sync
+
+up to date, audited 2 packages in 2s
+
+found 0 vulnerabilities
+
+PS C:\Users\Facundo\Documents\adivina_numero_secreto> node app.js
+¡Bienvenido a Adivina el número secreto!
+Intenta adivinar el número del 1 al 100.
+Ingresa un número: 50
+El número secreto es mayor. ¡Sigue intentando!
+Ingresa un número: 75
+El número secreto es menor. ¡Sigue intentando!
+Ingresa un número: 65
+El número secreto es menor. ¡Sigue intentando!
+Ingresa un número: 55
+El número secreto es menor. ¡Sigue intentando!
+Ingresa un número: 52
+El número secreto es mayor. ¡Sigue intentando!
+Ingresa un número: 54
+El número secreto es menor. ¡Sigue intentando!
+Ingresa un número: 53
+¡Felicitaciones! ¡Adivinaste el número secreto!
